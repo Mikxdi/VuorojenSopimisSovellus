3498fbdcb029eed8c715ecc0d5ba65f6c1489731
Mikxdi <mikko.lehtinen1996@gmail.com>
2019-03-28 13:58:42 +0200

    Tietokantaan lisääminen

--- application/suggestion/views.py
+++ application/suggestion/views.py
@@ -4,14 +4,17 @@ from application import app
 from flask import render_template, request
 
 @app.route("/suggestion/new/")
-def tasks_form():
+def suggestion_form():
     return render_template("suggestion/new.html")
 
 @app.route("/suggestion/", methods=["POST"])
-def tasks_create():
-    print(request.form.get("name"))
-    print(request.form.get("when"))
+def suggestion_create():
+    n = Suggestion(request.form.get("name"))
+    w = Suggestion(request.form.get("nick"))
 
+    db.session().add(n)
+    db.session().add(w)
+    db.session().commit()
   
     return "hello world!"
 
--- application/user/views.py
+++ application/user/views.py
@@ -2,12 +2,16 @@ from application import app
 from flask import render_template, request
 
 @app.route("/user/new/")
-def tasks_form():
+def user_form():
     return render_template("user/new.html")
 
 @app.route("/user/", methods=["POST"])
 def user_create():
-    U  = User(request.form.get("name"))
-    N  = User(request.form.get("nick"))
-   
+    n = User(request.form.get("name"))
+    nn = User(request.form.get("nick"))
+
+    db.session().add(n)
+    db.session().add(nn)
+    db.session().commit()
+ 
     return "Käyttäjä lisätty"
