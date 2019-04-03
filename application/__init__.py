59bbc2749303c169a41da6b4814c8e659489670b
Mikxdi <mikko.lehtinen1996@gmail.com>
2019-04-03 20:17:00 +0300

    Heroku ajantasalle

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,13 @@ from application import views
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///suggestion.db"
+import os
 
-app.config["SQLALCHEMY_ECHO"] = True
-
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 from application import views
@@ -33,5 +36,7 @@ login_manager.login_message = "Kirjaudu sisään"
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
-db.create_all()
+try:
+ db.create_all()
+except:
+ pass
