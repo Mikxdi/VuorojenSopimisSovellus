ee220878bda4a351b0564d206e597de1bbb6b6dd
Mikxdi <mikko.lehtinen1996@gmail.com>
2019-04-08 16:01:06 +0300

    Kirjautumissivun muutoksia

--- application/suggestion/views.py
+++ application/suggestion/views.py
@@ -4,12 +4,14 @@ from application import app
 from flask import render_template, request, url_for, redirect
 from application import db
 from application.suggestion.forms import SuggestionForm
-
+from flask_login import login_required
+ 
 @app.route("/suggestion/new/")
 def suggestion_form():
     return render_template("suggestion/new.html", form=SuggestionForm())
 
 @app.route("/suggestion/", methods=["POST"])
+@login_required
 def suggestion_create():
     form = SuggestionForm(request.form)
     if not form.validate():
