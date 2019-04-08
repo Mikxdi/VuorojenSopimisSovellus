739925f6b870eb36d768bb5efad0bfb80e6d7d80
Mikxdi <mikko.lehtinen1996@gmail.com>
2019-04-08 14:26:22 +0300

    Korjattu rekisteröityminen

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField 
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
   
+    class Meta:
+        csrf = False
+
+class RegisterForm(FlaskForm):
+    name = StringField("Nimi")
+    username = StringField("Käyttäjätunnus")
+    password = PasswordField("Salasana")
+
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
   
 from application import app
 from application.auth.models import User
 from application.auth.forms import LoginForm
+from application.auth.forms import RegisterForm
+from application import db
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -20,6 +22,32 @@ def auth_login():
 
     login_user(user)
     return redirect(url_for("index")) 
+
+@app.route("/auth/register", methods=["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form=RegisterForm())
+
+    form = RegisterForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/registerform.html", form=form)
+
+    u = User(form.name.data, form.username.data, form.password.data)
+
+
+
+    db.session().add(u)
+    db.session().commit()
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/loginform.html", form=form,
+                error="Käyttäjänimi tai salasana väärin")
+
+    print("Käyttäjä " + user.name + " tunnistettiin")
+    login_user(user)
+    return redirect(url_for("index"))
 
 @app.route("/auth/logout")
 def auth_logout():
